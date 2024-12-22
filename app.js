4f68a4b2884129fb7a3284ea24fc69355760cc98
bhavesh008 <bhavesh002jadhav@gmail.com>
2024-12-22 14:36:16 +0530

    first commit

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(cors({
     'https://hosteecommerce.vercel.app', 
     'https://mytalorzone-frontend-lake.vercel.app', 
     'https://mytalorzone-frontend-xeuvrqxyo-bhavesh-jadhavs-projects.vercel.app', 
-    'https://mytalorzone-frontend-git-main-bhavesh-jadhavs-projects.vercel.app'
+    'https://mytalorzone-frontend-git-main-bhavesh-jadhavs-projects.vercel.app',
+    'https://mytalorzone-frontend-a1k906h3h-bhavesh-jadhavs-projects.vercel.app/'
   ], // Frontend URLs
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
